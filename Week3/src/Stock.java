133965d6456dd9c419aac22cb6741b0c35a88b01
iglidraci <iglidraci98@gmail.com>
2021-11-01 23:38:25 +0100

    add Stock class

---
+++ Week3/src/Stock.java
@@ -0,0 +1,49 @@
+public class Stock {
+    private String symbol;
+    private String name;
+    private double previousClosingPrice;
+    private double currentPrice;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPreviousClosingPrice() {
+        return previousClosingPrice;
+    }
+
+    public void setPreviousClosingPrice(double previousClosingPrice) {
+        this.previousClosingPrice = previousClosingPrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
+        this.symbol = symbol;
+        this.name = name;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+    }
+
+    public double getChangePercent() {
+        return ((currentPrice-previousClosingPrice)/previousClosingPrice)*100;
+    }
+}
