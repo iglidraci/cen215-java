d61aba484edc91f9b751aa5df235cacda3e313b3
iglidraci <iglidraci98@gmail.com>
2021-11-10 22:36:04 +0100

    refactor

--- Week3/src/TestAccount.java
+++ Week3/src/TestAccount.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class TestAccount {
     public static void main(String[] args) {
         Account account = new Account(112, 20000, 0.045);
@@ -6,5 +8,10 @@ public class TestAccount {
         System.out.println("Current balance: " + account.getBalance());
         System.out.println("Monthly interest: " + account.getMonthlyInterest());
         System.out.println("Created on: " + account.getDateCreated());
+        var dateCreated = account.getDateCreated();
+        System.out.println(dateCreated);
+        dateCreated.setTime(200000);
+        System.out.println(dateCreated);
+        System.out.println(account.getDateCreated());
     }
 }
